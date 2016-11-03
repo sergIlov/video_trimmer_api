e11d54bec47683621713e8e78b51e1030fe3af3c
Sergey Ilov <serg.ilov@gmail.com>
2016-11-03 21:47:34 +0200

    api version moved to url

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# video_trimmer_api
+**Backend server that provides an API for trimming video.**
+
+User uploads video and defines timing parameters: start time, end time. After that video will be trimmed using that timing.
+User can trim one video as many times as he wants. 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  scope module: :v1, constraints: VersionConstraints.new(version: 1, default: true), defaults: {format: :json} do
+  scope module: :v1, path: 'v1', defaults: {format: :json} do
     resources :users, only: :create
     resources :videos, only: [:create, :index, :show]
     resources :tasks, only: [:create, :index, :show] do
       get :restart, on: :member
     end
-    
   end
 end
